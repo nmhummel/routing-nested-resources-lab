14ca2fa7770148cfd5001de9efb480db2229956e
nmhummel <natalie.hummel@gmail.com>
2021-01-12 11:35:15 -0600

    commit tree 55ad0700f09c444dcedf01c93e67f47e6d8c4484

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,34 @@
 class SongsController < ApplicationController
+  
   def index
-    @songs = Song.all
+    # redirects when artist not found
+    if params[:artist_id]
+      @artist = Artist.find_by_id(params[:artist_id])
+        if @artist.blank?
+          redirect_to artists_path, alert: "Artist not found." 
+        else
+          @songs = @artist.songs
+        end
+    else
+      @songs = Song.all
+    end
   end
 
+
   def show
-    @song = Song.find(params[:id])
+    # redirects to artists songs when artist song not found
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.blank?
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
+
+
 
   def new
     @song = Song.new
